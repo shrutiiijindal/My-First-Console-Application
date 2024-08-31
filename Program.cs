a23888eb06519a5fd0bbd7fd8a5614a66e24f863
Shruti <144424613+shrutiiijindal@users.noreply.github.com>
2024-09-01 00:19:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentdate= DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentdate:d}");
+Console.WriteLine($"{Environment.NewLine}Press any key to exit....");
+Console.ReadKey(true);
